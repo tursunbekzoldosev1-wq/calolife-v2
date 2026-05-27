7a8a4fd7e17174bb543e0f00d72893b9aba4923b
tursunbekzoldosev1-wq <tursunbekzoldosev1@gmail.com>
2026-05-27 09:41:12 +0600

    restore config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  plugins.push("react-native-worklets/plugin");
-  plugins.push("react-native-reanimated/plugin");
-
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-    plugins,
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
